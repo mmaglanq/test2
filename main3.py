d2df087e5ee426a2f9dc1a37a3ddc452645a73f5
jonelmawirat <jonel.mawirat@bugomi.com>
2017-05-06 05:58:26 +0000

    Second Commit

---
+++ main3.py
@@ -0,0 +1,13 @@
+for x in range(1,11):
+    print(x)
+
+list1 = [1,2,3,5,6,7,8,9,10]
+print(list1[5])
+
+list_len=len(list1)
+print (list1[-2])
+
+for x in range(0,9):
+    print(list1[x])
+    
+    
---
+++ main4.py
@@ -0,0 +1,13 @@
+y=0
+import csv
+exampleFile = open("ip_list.csv")
+exampleReader = csv.reader(exampleFile)
+
+for x in range(1,4):
+     exampleData = y[list(exampleReader)]
+     
+for x in range(1,4):
+     print(exampleData[x])
+
+    
+    
